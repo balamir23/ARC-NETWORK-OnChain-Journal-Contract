2a63049a9c565c1a193c124433d550b0707b6971
balamir <acimseda2@gmail.com>
2026-04-26 00:02:52 +0300

    Update ArcJournal.sol

--- Contracts/ArcJournal.sol
+++ Contracts/ArcJournal.sol
@@ -30,4 +30,5 @@ contract ArcJournal {
 function write(string memory _message) public {
     entries.push(Entry(msg.sender, _message, block.timestamp));
 }
+git commit -m "add write function to store messages"
 }
